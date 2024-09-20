3b333156e478c2319f13bca1eb5474ab00ac70d8
krishnamoorthy-v <clashkrishanmvp@gmail.com>
2024-09-20 21:45:43 +0530

    auth problem

--- student/validator.py
+++
@@ -1,9 +0,0 @@
-from .models import StudentModel
-
-import re
-
-
-def is_indian_mobile_number(number):
-    pattern = r'^[6-9]\d{9}$'
-    match = re.match(pattern, str(number) )
-    return bool(match)
